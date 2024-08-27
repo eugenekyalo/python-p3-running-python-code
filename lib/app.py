2c67bdec9fc53a47a61eb9976dfb0a41823899c3
Eugene Kyalo <yovlinpentdragonvii@gmail.com>
2024-08-27 19:55:20 +0300

    Completed running Python code assignment

--- lib/app.py
+++ lib/app.py
@@ -1,15 +1,1 @@
-# lib/app.py
 print("Hello World! Pass this test, please.")
-
-# lib/app.py
-print("Hello world!")
-print("Hello sun!")
-print("Hello sky!")
-
-# lib/app.py
-print("Hello world!", end=" ")
-print("Hello sun!", end="!! ")
-print("Hello sky!", end="!!!\n")
-
-def test_app_py_exists():
-    assert(path.exists("lib/app.py"))
